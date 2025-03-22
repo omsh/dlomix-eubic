f89996e7a94901618470c0ab07887e3b29522ddc
omsh <omar.shouman@gmail.com>
2025-03-22 23:46:47 +0200

    updates to intensity torch run + numerical stability + gradients

--- run_scripts/run_prosit_intensity_torch.py
+++ run_scripts/run_prosit_intensity_torch.py
@@ -1,13 +1,12 @@
-import os
-import sys
+import torch
+from tqdm import tqdm
 
-import torch.optim as optim
+from dlomix.constants import ALPHABET_UNMOD
 from dlomix.data import FragmentIonIntensityDataset
 from dlomix.losses.intensity_torch import masked_spectral_distance_torch
 from dlomix.models import PrositIntensityPredictorTorch
-from tqdm import tqdm
 
-# consider the use-case for starting from a saved model
+BATCH_SIZE = 128
 
 model = PrositIntensityPredictorTorch(
     seq_length=30,
@@ -20,55 +19,72 @@ model = PrositIntensityPredictorTorch(
     #     "PRECURSOR_CHARGE_KEY": "precursor_charge_onehot",
     # },
     with_termini=False,
+    alphabet=ALPHABET_UNMOD,
 )
 
-optimizer = optim.Adam(params=model.parameters(), lr=0.0001)
+optimizer = torch.optim.Adam(params=model.parameters(), lr=0.001)
 
 TRAIN_DATAPATH = "example_dataset/intensity/third_pool_processed_sample.parquet"
 
 d = FragmentIonIntensityDataset(
     data_source=TRAIN_DATAPATH,
     max_seq_len=30,
-    batch_size=8,
+    batch_size=BATCH_SIZE,
     val_ratio=0.2,
     # model_features=["collision_energy_aligned_normed", "precursor_charge_onehot"],
     sequence_column="modified_sequence",
     label_column="intensities_raw",
     # features_to_extract=["mod_loss", "delta_mass"],
-    dataset_type="pt"
+    dataset_type="pt",
+    alphabet=ALPHABET_UNMOD,
+    with_termini=False,
 )
 
-# model.compile(optimizer=optimizer, loss=masked_spectral_distance, metrics=["mse"])
 
-# weights_file = "run_scripts/output/prosit_intensity_test"
-# checkpoint = tf.keras.callbacks.ModelCheckpoint(
-#     weights_file, save_best_only=True, save_weights_only=True
-# )
-# decay = tf.keras.callbacks.ReduceLROnPlateau(
-#     monitor="val_loss", factor=0.1, patience=10, verbose=1, min_lr=0
-# )
-# early_stop = tf.keras.callbacks.EarlyStopping(patience=20)
-# callbacks = [checkpoint, early_stop, decay]
-
-# make training loop 
-
-model.train()
 loss_criterion = masked_spectral_distance_torch
 
-for epoch in tqdm(range(0, 19)):
+for epoch in tqdm(range(0, 100)):
     epoch_loss = 0
+    model.train()
+    data_size = len(d.tensor_train_data)
+    print(data_size)
     for batch in d.tensor_train_data:
         optimizer.zero_grad()
 
+        # output = model(batch["modified_sequence"])
         output = model(batch)
-        loss = loss_criterion(output, batch["intensities_raw"])
-        print(loss.item())
-        epoch_loss += loss.item()    
+        # print("output: ", output)
+        # print("label: ", batch["intensities_raw"])
+        loss = loss_criterion(batch["intensities_raw"], output)
+        # print(loss.item())
+        epoch_loss += loss.item()
 
-        loss.backward()         
+        loss.backward()
+
+        # Add before optimizer.step()
+        torch.nn.utils.clip_grad_norm_(
+            model.parameters(), max_norm=1, norm_type=2, error_if_nonfinite=False
+        )
         optimizer.step()
-    print(epoch_loss)
-    
+    print(epoch_loss / data_size)
+
+    # Validation phase.
+    model.eval()
+    val_loss_total = 0.0
+    with torch.no_grad():
+        val_data_size = len(d.tensor_val_data)
+        print(val_data_size)
+        for batch in d.tensor_val_data:
+            val_seq = batch["modified_sequence"]
+            val_label = batch["intensities_raw"]
+
+            val_pred_cs = model(val_seq)
+            val_loss = loss_criterion(val_label, val_pred_cs)
+            val_loss_total += val_loss.item()
+
+        avg_val_loss = val_loss_total / val_data_size
+    print(f"Epoch {epoch} Summary:  Validation Loss: {avg_val_loss:.4f}")
+
 
 # # to add test data, a pool for example
 # td = IntensityDataset(
